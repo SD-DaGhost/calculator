e9b7901c214795f24fa7c3f0550b44968908ccde
Araba Aramide <aramide231@gmail.com>
2023-09-12 03:26:52 +0100

    i made some changes

--- index.js
+++ index.js
@@ -63,17 +63,28 @@ percentage.addEventListener("click", function () {
 });
 
 
-const squareRoot = document.getElementById("squareRoot");
-squareRoot.addEventListener("click", function () {
-  const display = document.getElementById("display");
-  if (display && content) {
-    const currentValue = parseFloat(content);
-    if (!isNaN(currentValue) && currentValue >= 0) {
-      const result = Math.sqrt(currentValue); // 
-      content = result.toString();
-      display.value = content;
+// y
+document.addEventListener("DOMContentLoaded", function () {
+  var myElement = document.getElementById("Desktop1");
+  var colorButton = document.getElementById("change");
+  var randomColors = ["rgb(52, 152, 219)", "rgb(169, 79, 83)"];
+  var isInitialColor = true; // Flag to track the current state
+
+  // Store the initial color
+  var initialColor = myElement.style.background;
+
+  function toggleBackgroundColor() {
+    if (isInitialColor) {
+      const randomIndex = Math.floor(Math.random() * randomColors.length);
+      const randomColor = randomColors[randomIndex];
+      myElement.style.background = randomColor;
     } else {
-      alert("Invalid input for square root");
+      myElement.style.background = initialColor;
     }
+    
+    // Toggle the state flag
+    isInitialColor = !isInitialColor;
   }
+
+  colorButton.addEventListener("click", toggleBackgroundColor);
 });
